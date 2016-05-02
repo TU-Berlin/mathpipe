01fb0b413235547c51c0f2ed3734c6bea37481da
Moritz Schubotz <wiki@physikerwelt.de>
2016-05-02 00:23:54 -0400

    Get hash from tex

--- test/index.js
+++ test/index.js
@@ -1,22 +1,12 @@
 "use strict";
 var assert = require('assert');
 var pipe = require('../');
-var testcases = [
-    {input: '', options: '', out: ''},
-    {
-        input: 'This is a test',
-        options: {},
-        out: 'This is a test'
-    }
-];
+var testcases = require('./files/hash.json');
 
 describe('Index', function () {
     testcases.forEach(function (tc) {
-        var input = tc.input;
-        var options = tc.options;
-        var output = tc.out;
-        it('should correctly replace ' + JSON.stringify(input), function () {
-            assert.deepEqual(pipe.mathpipe(input, options), output);
+        it('should correctly process ' + JSON.stringify(tc), function () {
+            assert.deepEqual(pipe.getHash(tc.input), tc.inputhash);
         });
     });
 });
