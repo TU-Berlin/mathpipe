115bb65d40f77473c5821b711fc8a96a1f94c64f
Moritz Schubotz <wiki@physikerwelt.de>
2016-05-02 01:23:42 -0400

    Add folder creation functionality

--- test/index.js
+++ test/index.js
@@ -5,8 +5,13 @@ var testcases = require('./files/hash.json');
 
 describe('Index', function () {
     testcases.forEach(function (tc) {
-        it('should correctly process ' + JSON.stringify(tc), function () {
+        it('should calculate hash for ' + JSON.stringify(tc.input), function () {
             assert.deepEqual(pipe.getHash(tc.input), tc.inputhash);
+        });
+        it('should create dirs ' + JSON.stringify(tc.input), function (done) {
+            pipe.createFolder(tc.inputhash,'/tmp', function () {
+                done();
+            })
         });
     });
 });
