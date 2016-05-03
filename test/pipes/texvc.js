05bd5e965ee78575b4ffdf2e027873c7050e3c53
physikerwelt <wiki@physikerwelt.de>
2016-05-03 13:43:55 -0400

    Test invalid input with texvc
    Simplfy texvc code

--- test/pipes/texvc.js
+++ test/pipes/texvc.js
@@ -1,12 +1,22 @@
 "use strict";
 var texvc = require('../../lib/pipes/texvc.js');
 var testcases = require('../files/hash.json');
+var invalidTests = require('../files/invalid.json');
 var pipe = require('../..');
+var assert = require('assert');
 
 describe('texvc', function () {
     testcases.forEach(function (tc) {
-        it('should calculate hash for ' + JSON.stringify(tc.input), function () {
+        it('should process ' + JSON.stringify(tc.input), function () {
            return texvc(pipe.getFolder(tc.input, '/tmp'));
+        });
+    });
+    invalidTests.forEach(function (tc) {
+        it('should process invalid ' + JSON.stringify(tc.input), function () {
+            return texvc(pipe.getFolder(tc.input, '/tmp')).catch(function(e){
+                assert.equal(e.name,'Error');
+                assert.ok(e.message.startsWith('texvc failed'));
+            });
         });
     });
 });
