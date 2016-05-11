61bf30afcf179edf7586a751d863dd3f12477c90
Moritz Schubotz <wiki@physikerwelt.de>
2016-05-11 01:36:55 -0400

    Process firefox renderings strictly sequential
    The driver is passed from one request to the next.

--- test/pipes/firefox.js
+++ test/pipes/firefox.js
@@ -4,6 +4,6 @@ var firefox = require('../../lib/pipes/firefox.js');
 describe('firefox', function () {
     it('convert mml', function(){
         this.timeout(50000);
-        return firefox.snapshot(__dirname+'/../files/mj.mml');
+        return firefox.snapshot(firefox.getDriver(),__dirname+'/../files/mj.mml');
     });
 });
